3c4d27f0a0c6104b1770b7fddbc3793f53352d68
Rudy Huezo <rudy.huezo@goexactly.com>
2018-07-02 08:54:46 -0700

    Finished noaa api script

--- utils/noaa_api.py
+++ utils/noaa_api.py
@@ -14,12 +14,14 @@ tree_ring_sgi_url = 'https://www.ncdc.noaa.gov/paleo-search/study/search.json?da
 tree_ring_width_url = 'https://www.ncdc.noaa.gov/paleo-search/study/search.json?dataPublisher=NOAA&dataTypeId=18&cvWhats=physical%20property%3Elength%3Etotal%20ring%20width&headersOnly=true'
 bundling_url = 'https://www.ncdc.noaa.gov/paleo-search/data/search.json?xmlId={}'
 
-BREAK_LOOPS = 100
+BREAK_LOOPS = 20
+
+TREE_RING_WIDTH_RESULTS = 'results/tree_ring_width/{}'
+TREE_RING_SGI_RESULTS = 'results/tree_ring_sgi/{}'
 
 
 def load_json(url): 
     return json.loads(req.get(url).content)
-
 
 def get_xmlid(json_content): 
     return json_content['xmlId']
@@ -27,43 +29,67 @@ def get_xmlid(json_content):
 def get_bundling_status(json_content): 
     return json_content['statusUrl']
 
-def bundle_data(xmlid): 
+def get_bundle_url(xmlid): 
     return get_bundling_status(load_json(bundling_url.format(xmlid)))
 
 def check_bundling_status(status_url, break_loops=BREAK_LOOPS):     
     loops = 0 
     while True:         
         json_content = load_json(status_url)
+        status = json_content['status']
+        print "\tBundling status: {}".format(status)
 
-        if(json_content['status'] == 'complete'): 
+        if(status == 'complete'): 
             return json_content['archive']
 
         if(loops > break_loops): 
-            print "\nReached max loops and status was never complete.\n"
+            print "\n\tReached max loops and job was never complete.\n"
             return False 
         time.sleep(2)
+        print "\t\tSleeping for 2 seconds..."
         loops += 1
 
-
-# import requests, zipfile, io
-# r = requests.get(zip_file_url)
-
-# z.extractall()
-
 def get_bundle(bundle_url, directory): 
-    output_zip_path = os.path.join(directory, bundle_url.split('/')[-1])
-    z = zipfile.ZipFile(io.BytesIO(req.get(bundle_url).content))
-    z.extractall(directory)
+    zip_filename = bundle_url.split('/')[-1]
+    output_zip_path = os.path.join(directory, zip_filename)
+
+    with open(output_zip_path, 'w') as z: 
+        z.write(req.get(bundle_url).content)        
+        print "\tDownloaded {}".format(zip_filename)
     return True
 
-get_bundle(
-    check_bundling_status('https://www.ncdc.noaa.gov/paleo-search/data/status.json?jobId=2093648196_2018-06-22'),
-    'results'    
-)
 
+tree_ring_width_data = load_json(tree_ring_width_url)['study']
+tree_ring_sgi_data = load_json(tree_ring_sgi_url)['study']
 
+for i, study in enumerate(tree_ring_width_data): 
+    print '{} out of {} studies\n'.format(i + 1, len(tree_ring_width_data))
+    xmlid = get_xmlid(study)
+    
+    print "Current study: {}".format(xmlid)
 
+    results_path = TREE_RING_WIDTH_RESULTS.format(xmlid)
+    bundle_url = get_bundle_url(xmlid)
+    current_status = check_bundling_status(bundle_url)
 
+    if current_status:
+        if not os.path.exists(results_path): 
+            os.makedirs(results_path)
+        get_bundle(current_status, results_path)
 
+print 
 
+for i, study in enumerate(tree_ring_sgi_data): 
+    print '{} out of {} studies\n'.format(i + 1, len(tree_ring_sgi_data))
+    xmlid = get_xmlid(study)
+    
+    print "Current study: {}".format(xmlid)
 
+    results_path = TREE_RING_SGI_RESULTS.format(xmlid)
+    bundle_url = get_bundle_url(xmlid)
+    current_status = check_bundling_status(bundle_url)
+
+    if current_status:
+        if not os.path.exists(results_path): 
+            os.makedirs(results_path)
+        get_bundle(current_status, results_path)
