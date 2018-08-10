1a1cdb88173d65d1f46b68ffa28a99695c1f9dc0
Rudy Huezo <rahuezo@ucdavis.edu>
2018-08-10 07:15:20 -0700

    Added study packager to rwl_finder

--- utils/finders.py
+++ utils/finders.py
@@ -18,6 +18,7 @@ def rwl_finder(path):
                 package['paleodata'].extend(files)
         
             if not all([len(package[key]) > 0 for key in package]): 
+                print "ERROR "*10
                 continue
             yield package
         
