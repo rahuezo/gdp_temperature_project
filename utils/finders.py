a0e545b13790c8b283c29dbb7c7d9f5164588afc
Rudy Huezo <rahuezo@ucdavis.edu>
2018-08-10 06:48:47 -0700

    Testing finders.py

--- utils/finders.py
+++ utils/finders.py
@@ -2,7 +2,26 @@ import os
 
 
 def rwl_finder(path): 
-    for root, dirs, files in os.walk(path): 
-        for f in files: 
-            if f.lower().endswith('.rwl'): 
-                yield os.path.join(root, f)
+    path_dirs = [os.path.join(path, d) for d in os.listdir(path)]
+
+    for pdir in path_dirs: 
+        package = {'paleoData':[], 'metadata':[], 'correlation':[]}
+
+        for root, dirs, files in os.walk(pdir): 
+            if root.endswith('metadata'): 
+                package['metadata'].extend(files)
+            
+            if root.endswith('correlation-stats'): 
+                package['correlation'].extend(files)
+
+            if 'measurements' in root and all([f.endswith('.rwl') for f in files]): 
+                package['paleoData'].extend(files)
+        
+        print package
+            # for f in files: 
+            #     printf
+                # if f.lower().endswith('.rwl'): 
+                #     yield os.path.join(root, f)
+
+
+rwl_finder(r"E:\gdp_temperature_project\results\treering_data_width")
