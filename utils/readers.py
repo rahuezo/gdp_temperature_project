cde16216203b76c8de4afd13c947a4b733cd9188
Rudy Huezo <rahuezo@ucdavis.edu>
2018-08-13 06:11:59 -0700

    Working on rwl_finder

--- utils/readers.py
+++ utils/readers.py
@@ -1,12 +1,95 @@
 import re
 import codecs
 import chardet
+import json
 
 CENTI_MM = 0.01
 MILLI_MM = 0.001
 
 
+def get_first(l): 
+    if len(l) > 0: 
+        return l[0]
+    return None
+
+#         self.site_id = header[0][:6].strip()
+#         self.site_name = header[0][9:61].strip()
+#         self.species_id = header[0][61:65].strip()
+#         self.location = header[1][9:22].strip()
+#         self.species = header[1][22:40].strip()        
+#         self.elevation = header[1][40:45].strip().lower().replace('m', '')
+
+#         if len(self.elevation.lower().strip()) == 0:
+#             self.elevation = 0
+
+#         self.coordinates = header[1][47:57].strip().replace('_', '')
+#         self.year_range = header[1][67:76].strip().replace(' ', '-')
+#         self.investigator = header[2][9:72].strip()
+#         self.comp_date =
+# {'correlation': [u'brit043cof.txt'], 'paleodata': [u'brit043.rwl'], 'metadata': [u'noaa-tree-2670.json']}
+
 class RwlReader: 
+    def __init__(self, package): 
+        self.metadata_file = get_first(package['metadata'])
+        self.correlation_file = get_first(package['correlation'])
+        self.paleodata_file = get_first(package['paleodata'])
+        self.site_id = self.site_name = self.species = self.species_id = self.elevation = self.coordinates = self.year_range = None
+
+        self.set_header_from_metadata()
+
+        print "site id: ", self.site_id
+        print "site name: ", self.site_name
+        print "species: ", self.species
+        print "species id: ", self.species_id
+        print "elevation: ", self.elevation
+        print "coords: ", self.coordinates
+        print "year range: ", self.year_range
+
+
+    def set_header_from_metadata(self): 
+        with open(self.metadata_file, 'rb') as mf: 
+            metadata = json.load(mf)
+            if 'site' in metadata: 
+                site_data = get_first(metadata['site'])
+                if site_data: 
+                    if 'siteName' in site_data: 
+                        site_name = site_data['siteName']                 
+                        if site_name and len(site_name) and self.site_name == None: 
+                            self.site_name = site_name.title()
+                    if 'geo' in site_data: 
+                        geo = site_data['geo']
+                        if 'geometry' in geo: 
+                            geometry = geo['geometry']
+                            if 'coordinates' in geometry: 
+                                coordinates = geometry['coordinates']
+                                if coordinates and len(coordinates) >= 2 and self.coordinates == None: 
+                                    self.coordinates = coordinates
+                        if 'properties' in geo: 
+                            properties = geo['properties']
+                            if 'maxElevationMeters' in properties: 
+                                max_elevation = properties['maxElevationMeters']
+                                if max_elevation and len(str(max_elevation)) and self.elevation == None: 
+                                    self.elevation = max_elevation
+                    if 'paleoData' in site_data: 
+                        paleodata = get_first(site_data['paleoData'])
+                        if 'earliestYear' in paleodata and 'mostRecentYear' in paleodata: 
+                            first_year = paleodata['earliestYear']
+                            last_year = paleodata['mostRecentYear']
+                            if first_year and last_year and len(str(first_year)) and len(str(last_year)) and self.year_range == None: 
+                                self.year_range = [first_year, last_year]
+
+                        if 'species' in paleodata: 
+                            species_data = get_first(paleodata['species'])
+                            if 'speciesCode' in species_data: 
+                                species_code = species_data['speciesCode']
+                                if species_code and len(species_code) and self.species_id == None: 
+                                    self.species_id = species_code
+                            if 'commonName' in species_data: 
+                                common_name = species_data['commonName']
+                                if common_name and len(common_name) and self.species == None: 
+                                    self.species = ' '.join(common_name)
+
+
     @staticmethod
     def get_units(content): 
         if '-9999' in content: 
@@ -15,7 +98,7 @@ class RwlReader:
             return (CENTI_MM, 999)
         else: 
             return 'NA'
-
+    
     @staticmethod
     def detect_encoding(f, nlines=4): 
         i = 0
@@ -27,120 +110,165 @@ class RwlReader:
                 content.append(line)            
                 i += 1
         encoding = chardet.detect(''.join(content))['encoding']
-
-        # print "Encoding: {}".format(encoding)
         return encoding
-
-            
 
     @staticmethod
     def get_content(f): 
-        # with open(f, 'r') as fobj: 
-        #     return fobj.read().encode('ascii', errors='ignore') # OLD WAY
         with codecs.open(f, 'r', encoding=RwlReader.detect_encoding(f)) as fobj: 
             return fobj.read().replace('\r\n', '\n').replace('\r', '\n')
 
-    @staticmethod
-    def get_header(f): 
-        return RwlReader.get_content(f).split('\n')[:3]
 
-    @staticmethod
-    def split_row(row): 
-        core_id = row[:8].strip()
-        decade = row[8:12].strip()
+package = {}
+package["metadata"] = [r"C:\Users\rahue\Programming\gdp_temperature_project\results\treering_width_data\11834\864602848_2018-07-09\metadata\noaa-tree-13816.json"]
+package['paleodata'] = [r"C:\Users\rahue\Programming\gdp_temperature_project\results\treering_width_data\11834\864602848_2018-07-09\data\pub\data\paleo\treering\measurements\northamerica\usa\nc020.rwl"]
+package['correlation'] = [r"C:\Users\rahue\Programming\gdp_temperature_project\results\treering_width_data\11834\864602848_2018-07-09\data\pub\data\paleo\treering\measurements\correlation-stats\nc020.txt"]
 
-        try: 
-            decade = int(decade)
-        except ValueError: 
-            return False
 
-        data = row[12:].strip().split()
-        return core_id, decade, data
 
-    def __init__(self, f): 
-        self.f = f
-        self.content = RwlReader.get_content(self.f)
-        self.units, self.missing_value_id = RwlReader.get_units(self.content)
-        print "Before metadata"
-        self.get_metadata()
-        print "After metadata"
+reader = RwlReader(package)
+# reader.set_header_from_metadata()
 
-    def get_metadata(self): 
-        header = self.get_header(self.f) 
-        print "Header"
-        print header
-        print "After Header"
-        self.site_id = header[0][:6].strip()
-        self.site_name = header[0][9:61].strip()
-        self.species_id = header[0][61:65].strip()
-        self.location = header[1][9:22].strip()
-        self.species = header[1][22:40].strip()        
-        self.elevation = header[1][40:45].strip().lower().replace('m', '')
 
-        if len(self.elevation.lower().strip()) == 0:
-            self.elevation = 0
 
-        self.coordinates = header[1][47:57].strip().replace('_', '')
-        self.year_range = header[1][67:76].strip().replace(' ', '-')
-        self.investigator = header[2][9:72].strip()
-        self.comp_date = header[2][72:80].strip() 
 
-    def get_db_row(self): 
-        return (self.site_id, self.site_name, self.species_id, self.location, 
-            self.species, self.elevation, self.coordinates, self.units, self.missing_value_id)
 
-    def get_data(self):
-        for row in self.content.split('\n')[3:-1]:
 
-            columns = RwlReader.split_row(row)
 
-            if columns: 
-                core_id, decade, data = columns
-            else: 
-                # print "ValueError: {}\n".format(row)
-                continue
+# class RwlReader: 
+#     @staticmethod
+#     def get_units(content): 
+#         if '-9999' in content: 
+#             return (MILLI_MM, -9999)
+#         elif '999': 
+#             return (CENTI_MM, 999)
+#         else: 
+#             return 'NA'
 
-            for i, ring_width in enumerate(data):
-                try:  
-                    ring_width = int(ring_width)
-                except: 
-                    continue
-                if ring_width != self.missing_value_id:             
-                    year = decade + i
-                    yield self.get_db_row() + (core_id, year, ring_width)
+#     @staticmethod
+#     def detect_encoding(f, nlines=4): 
+#         i = 0
+#         with open(f, 'r') as fobj:
+#             content = []
+#             for line in fobj: 
+#                 if i > nlines: 
+#                     break 
+#                 content.append(line)            
+#                 i += 1
+#         encoding = chardet.detect(''.join(content))['encoding']
 
-    def print_metadata(self):
-        print """
-        ******************* HEADER *******************
+#         # print "Encoding: {}".format(encoding)
+#         return encoding
+
+            
+
+#     @staticmethod
+#     def get_content(f): 
+#         # with open(f, 'r') as fobj: 
+#         #     return fobj.read().encode('ascii', errors='ignore') # OLD WAY
+#         with codecs.open(f, 'r', encoding=RwlReader.detect_encoding(f)) as fobj: 
+#             return fobj.read().replace('\r\n', '\n').replace('\r', '\n')
+
+#     @staticmethod
+#     def get_header(f): 
+#         return RwlReader.get_content(f).split('\n')[:3]
+
+#     @staticmethod
+#     def split_row(row): 
+#         core_id = row[:8].strip()
+#         decade = row[8:12].strip()
+
+#         try: 
+#             decade = int(decade)
+#         except ValueError: 
+#             return False
+
+#         data = row[12:].strip().split()
+#         return core_id, decade, data
+
+#     def __init__(self, f): 
+#         self.f = f
+#         self.content = RwlReader.get_content(self.f)
+#         self.units, self.missing_value_id = RwlReader.get_units(self.content)
+#         print "Before metadata"
+#         self.get_metadata()
+#         print "After metadata"
+
+#     def get_metadata(self): 
+#         header = self.get_header(self.f) 
+#         print "Header"
+#         print header
+#         print "After Header"
+#         self.site_id = header[0][:6].strip()
+#         self.site_name = header[0][9:61].strip()
+#         self.species_id = header[0][61:65].strip()
+#         self.location = header[1][9:22].strip()
+#         self.species = header[1][22:40].strip()        
+#         self.elevation = header[1][40:45].strip().lower().replace('m', '')
+
+#         if len(self.elevation.lower().strip()) == 0:
+#             self.elevation = 0
+
+#         self.coordinates = header[1][47:57].strip().replace('_', '')
+#         self.year_range = header[1][67:76].strip().replace(' ', '-')
+#         self.investigator = header[2][9:72].strip()
+#         self.comp_date = header[2][72:80].strip() 
+
+#     def get_db_row(self): 
+#         return (self.site_id, self.site_name, self.species_id, self.location, 
+#             self.species, self.elevation, self.coordinates, self.units, self.missing_value_id)
+
+#     def get_data(self):
+#         for row in self.content.split('\n')[3:-1]:
+
+#             columns = RwlReader.split_row(row)
+
+#             if columns: 
+#                 core_id, decade, data = columns
+#             else: 
+#                 # print "ValueError: {}\n".format(row)
+#                 continue
+
+#             for i, ring_width in enumerate(data):
+#                 try:  
+#                     ring_width = int(ring_width)
+#                 except: 
+#                     continue
+#                 if ring_width != self.missing_value_id:             
+#                     year = decade + i
+#                     yield self.get_db_row() + (core_id, year, ring_width)
+
+#     def print_metadata(self):
+#         print """
+#         ******************* HEADER *******************
         
-        Site ID: {} Site Name: {}   Location: {}    Investigator: {}
+#         Site ID: {} Site Name: {}   Location: {}    Investigator: {}
 
-        Species: {} Species id: {}    Units: {}mm
+#         Species: {} Species id: {}    Units: {}mm
 
-        Elevation: {}   Coordinates: {} Year Range: {}  Comp. Date: {}
+#         Elevation: {}   Coordinates: {} Year Range: {}  Comp. Date: {}
 
-        File: {}
+#         File: {}
 
-        **********************************************
-        """.format(self.site_id, self.site_name, self.location, 
-            self.investigator, self.species, self.species_id,
-            self.units, self.elevation, self.coordinates, self.year_range, self.comp_date, self.f)
+#         **********************************************
+#         """.format(self.site_id, self.site_name, self.location, 
+#             self.investigator, self.species, self.species_id,
+#             self.units, self.elevation, self.coordinates, self.year_range, self.comp_date, self.f)
 
 
-class CrnReader(RwlReader): 
-    @staticmethod
-    def split_row(row): 
-        site_id = row[:6]
-        decade = int(row[6:10].strip())
-        records = [(int(col[:4]), int(col[4:])) for col in re.findall(r'.{7}', row[10:])]
-        return site_id, decade, records
+# class CrnReader(RwlReader): 
+#     @staticmethod
+#     def split_row(row): 
+#         site_id = row[:6]
+#         decade = int(row[6:10].strip())
+#         records = [(int(col[:4]), int(col[4:])) for col in re.findall(r'.{7}', row[10:])]
+#         return site_id, decade, records
 
-    def __init__(self, f): 
-        self.f = f
-        RwlReader.__init__(self, self.f)
+#     def __init__(self, f): 
+#         self.f = f
+#         RwlReader.__init__(self, self.f)
 
-    def get_data(self):
-        for row in self.content.split('\n')[3:-1]:
-            print CrnReader.split_row(row)
+#     def get_data(self):
+#         for row in self.content.split('\n')[3:-1]:
+#             print CrnReader.split_row(row)
 
     
