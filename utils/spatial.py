e65a8564a9258526d45ed9451725754da432c6ba
Rudy Huezo <rahuezo@ucdavis.edu>
2018-09-20 07:49:02 -0700

    Working on cru reading and coord matching.

--- utils/spatial.py
+++ utils/spatial.py
@@ -1,48 +1,78 @@
-from math import sqrt
+import tkFileDialog as fd
+import csv
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-def make_grid(lat_min=-89.75, lon_min=-179.75, step=0.5):
-    lat_max = 180 + lat_min
-    lon_max = 360 + lon_min
-
-    lat_range = np.arange(lat_min, lat_max + step, step)
-    lon_range = np.arange(lon_min, lon_max + step, step)
-
-    grid = {}
-
-    for i, lon in enumerate(lon_range): 
-        for j, lat in enumerate(lat_range):
-            key = (i, j)
-            grid[key] = (lon, lat)
-    return grid
+LAT_MIN = -89.75
+LON_MIN = -179.75
+STR_LEN = 8
 
 
-def get_closest_cell(lon, lat, grid): 
-    mkey = min(grid, key=lambda k: sqrt((grid.get(k)[0] - lon)**2 + (grid.get(k)[1] - lat)**2))   
-    return mkey, grid[mkey]
+def round_coords(coords): 
+    lat, lon = coords
+    rlat, rlon = round(lat), round(lon)
+
+    def tug(x, rx): 
+        return rx + 0.25 if rx < x else rx - 0.25
+    return tug(lat, rlat), tug(lon, rlon)
+
+def get_rowcol(rcoords, multiplier=2): 
+    rlat, rlon = rcoords
+
+    def transform(x, xmin): 
+        return x + abs(xmin)
+    return int(transform(rlat, LAT_MIN) * multiplier), int(transform(rlon, LON_MIN) * multiplier)
+
+def get_value_from_row(row_str, col):
+    return row_str[col*STR_LEN : col*STR_LEN + STR_LEN]
+
+def read_cru(p): 
+    i = 0
+    for line in open(p, 'r'): 
+        yield i, line
+        i = 0 if i == 359 else i + 1
+
+def load_coords(p, header=True): 
+    with open(p, 'rb') as f: 
+        if header: f.next()
+
+        groups = {}
+
+        for row in csv.reader(f, delimiter=','): 
+            rcoords = round_coords(map(float, row))
+            row, col = get_rowcol(rcoords)
+
+            if row not in groups: 
+                groups[row] = [rcoords]
+            else: 
+                groups[row].append(rcoords)
+        return groups
+    
+
+coordsp = fd.askopenfilename(title="Choose coords file")
+
+groups = load_coords(coordsp)
+for i in sorted(groups.iterkeys()): 
+    print i, len(groups[i])
 
 
-def cru_dat_to_grid(p): 
-    grid = {}
-    with open(p, 'r') as f: 
-        for i, row in enumerate(f.readlines()[:360]):
-            columns = row.strip().split('    ')
+crup = fd.askopenfilename(title="Choose cru file")
+for line in read_cru(crup): 
+    i, values = line
 
-            for j, col in enumerate(columns): 
-                grid[(i, j)] = col
+    if i in groups: 
+        for coord in groups[i]: 
+            row, col = get_rowcol(coord)
 
-    return grid
+            print "Coord: ", coord, "Value: ", get_value_from_row(values, col)
 
+
+
+#     print line[0]
+
+    # if line[0] == 359: 
+    #     break
 
 
 # p = r'C:\Users\Rudy\Downloads\cru_ts4.01.2011.2016.pre.dat.gz'
-
-# g = cru_dat_to_grid(p)
-
-# print g
 
 # var a = csv.select(x => new coordinate(x[0], x[1]))
 #     .groupby(x => x.row)
@@ -75,6 +105,7 @@ def cru_dat_to_grid(p):
 #     public double col {get { return _col == int.min ? _col = (this.rounded_lng + 179.75) * 2 : _col; } }
 #     public List<int> precip {get { return _precip; }}
 # }
+
 
 
 
